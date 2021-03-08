2446537313b4d297387ef4cd26abc5c384024995
Ashwin Witbooi <ashwin.witbooi@gmail.com>
2021-03-08 21:20:18 +0200

    initial commit

---
+++ src/test/java/za/co/ashtech/trek/TrekApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.ashtech.trek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
