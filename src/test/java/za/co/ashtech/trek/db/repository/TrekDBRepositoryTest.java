76f9e343b9e63393cc7e1021b6d798e679a751bd
Ashwin Witbooi <ashwin.witbooi@gmail.com>
2021-03-14 12:50:41 +0200

    add db repo unit test

---
+++ src/test/java/za/co/ashtech/trek/db/repository/TrekDBRepositoryTest.java
@@ -0,0 +1,51 @@
+package za.co.ashtech.trek.db.repository;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import za.co.ashtech.trek.db.entity.TrailEntity;
+import za.co.ashtech.trek.util.TestDataUtil;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
+ class TrekDBRepositoryTest {
+	
+	@Autowired
+	TrekDBRepository trekDBRepository;
+	static String trailName = null;
+	
+	@BeforeAll
+	public static void setUp() {
+		trailName = TestDataUtil.getTrailname();
+	}
+	
+	@BeforeEach
+	void validate() {
+		assertNotNull(trekDBRepository);
+	}
+	
+	@Test
+	@Order(1) 
+	void getTrailTest() throws Exception{		
+		TrailEntity te = new TrailEntity();
+		te.setDecription("decription");
+		te.setLength("5KM");
+		te.setLevel("Beginner");
+		te.setLocation("Bellville");
+		te.setName(trailName);
+		te.setStatus("O");
+
+		trekDBRepository.save(te);
+		
+		assertNotNull(trekDBRepository.findByName(trailName));
+	}
+	
+}
